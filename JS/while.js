54375aa235830afef1d905d61b8d2a7fd19c50ee
desktop-o0gpqr4\antsh <ants.hernandez@gmail.com>
2022-02-09 16:42:35 -0600

    updateing worksheet files

--- JS/while.js
+++ JS/while.js
@@ -14,22 +14,21 @@ num = multiply;
 
 // generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop
 // nside of the loop your code should generate another random number between 1 and 5
-
+// while we go from top to bottom until there is 1 ice cream cone left i figured it was best to Cannot sell you 6 cones I only have 1 left
 // This is how you get a random number between 50 and 100
+
 var allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
 var random = Math.floor(Math.random() * 5) + 1;
-
 do{
     allCones = allCones - random
     console.log(random)
-    console.log("hurry up they going quick")
     console.log('We have '+ allCones +' '+ "left")
 
     if(allCones < random|| allCones === 0){
-        console.log("Sorry we don't have enough to fill that order ")
+        console.log(("Cannot sell you") +' '+`${allCones} cones I only have 1 left`)
         break;
     }
-}while (allCones>= 1)
+}while (allCones >= 1)
 
 //

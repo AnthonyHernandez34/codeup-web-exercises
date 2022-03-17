e02801ff15b25790d411bd4ec84785bb1c41d2a6
desktop-o0gpqr4\antsh <ants.hernandez@gmail.com>
2022-03-17 16:55:29 -0500

    updating work

--- JS/keys.js
+++ JS/keys.js
@@ -1,3 +1,5 @@
 const OWM_KEY = "f6a3f79aed887ba2f5e2beb80d53d550";
 
-const OWM_KEY = "9ca006c4d4mshb87f46688eb6a2ap1c4345jsnaf587db24ace";
+const APPI_KEY = "9ca006c4d4mshb87f46688eb6a2ap1c4345jsnaf587db24ace";
+
+const APP_KEY = "pk.eyJ1IjoicmVjb25kbyIsImEiOiJjbDB2ZGwwc3IwMnBqM2RwZTRmd3NmbzJsIn0.TPOtB76P8K-6E-SK5T-XyA";
